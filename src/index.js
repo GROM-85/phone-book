36bcabcbb9d8450c31c79e2f4871b65fd94010ac
GROM-85 <richfron69@gmail.com>
2023-03-28 16:58:20 +0200

    fix path

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Toaster } from 'react-hot-toast';
 
-//  goit-react-hw-08-phonebook/
+//  phonebook/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/'>
+        <BrowserRouter basename='phonebook'>
           <App />
         </BrowserRouter>
         <Toaster position="top-center" reverseOrder={false} autoClose={2000} />
